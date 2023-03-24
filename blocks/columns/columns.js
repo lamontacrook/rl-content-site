5ca9f4a4c884dbbda033772e799ee0251ab685e2
Lamont Crook <lamont@adobe.com>
2023-03-24 15:09:40 -0600

    text photo grid

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -3,7 +3,7 @@ export default function decorate(block) {
   block.classList.add(`columns-${cols.length}-cols`);
 
   const h2 = block.querySelector('h2');
-  console.log(h2.innerHTML.includes('&amp;'));
+  
   if(h2.innerHTML.includes('&amp;')) {
     h2.innerHTML = h2.innerHTML.replace('&amp;', '<br />&amp;<br />');
   }
